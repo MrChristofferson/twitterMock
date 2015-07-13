5f962491c40bf29585777f806763919ed82f24eb
Chris Vandenberg <chrisvan0848@gmail.com>
2015-07-13 11:23:53 -0700

    Added prettiness

--- scripts/main.js
+++ scripts/main.js
@@ -3,6 +3,11 @@
 $(function() {
 
 
+	$('body').on('click', 'button', function () {
+		var value = $('.input-text').val()
+		$('ul').append("<li>" + value + "</li>")
+		$('.input-text').val('')
+	})
 
 
 });
